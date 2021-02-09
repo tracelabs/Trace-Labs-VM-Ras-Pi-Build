4277c6e81f0eb6a8983b6301e54fefe87532c3ef
Tom Hocker <tomhocker@protonmail.com>
2021-02-09 10:16:09 -0500

    just a test

--- updater.sh
+++ updater.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-
+#this is a test comment
 # this script will pull the most recent repo for updating
 # the repo includes a script that updates the system and tools
 # as well as plugs in the most recent firefox bookmarks
 cd /home/osint
 
-if [ -d "/home/osint/Trace-Labs-VM-Ras-Pi-Updater" ]; then        
+#if [ -d "/home/osint/Trace-Labs-VM-Ras-Pi-Updater" ]; then        
 	
-		cd /home/osint/Trace-Labs-VM-Ras-Pi-Updater
-        	git pull https://github.com/tracelabs/Trace-Labs-VM-Ras-Pi-Updater.git
-	else
-		git clone https://github.com/tracelabs/Trace-Labs-VM-Ras-Pi-Updater.git
-		cd /home/osint/Trace-Labs-VM-Ras-Pi-Updater
-	fi
- chmod +x /home/osint/Trace-Labs-VM-Ras-Pi-Updater/update-system.sh
- sudo /home/osint/Trace-Labs-VM-Ras-Pi-Updater/update-system.sh
+#		cd /home/osint/Trace-Labs-VM-Ras-Pi-Updater
+ #       	git pull https://github.com/tracelabs/Trace-Labs-VM-Ras-Pi-Updater.git
+#	else
+#		git clone https://github.com/tracelabs/Trace-Labs-VM-Ras-Pi-Updater.git
+#		cd /home/osint/Trace-Labs-VM-Ras-Pi-Updater
+#	fi
+ 
+ wget -O update-system.sh https://raw.githubusercontent.com/tracelabs/Trace-Labs-VM-Ras-Pi-Updater/main/update-system.sh
+ chmod +x /home/osint/update-system.sh
+ sudo /home/osint/update-system.sh
  #sudo cp distribution.ini /usr/share/firefox-esr/distribution/
- sudo mv "osint bookmarks.html" ~/Desktop
- sudo rm -rf /home/osint/Trace-Labs-VM-Ras-Pi-Updater
+ #sudo mv "osint bookmarks.html" ~/Desktop
+ wget -O osint-bookmarks.html https://raw.githubusercontent.com/tracelabs/Trace-Labs-VM-Ras-Pi-Updater/main/osint%20bookmarks.html 
+ sudo rm -rf /home/osint/update-system.sh
