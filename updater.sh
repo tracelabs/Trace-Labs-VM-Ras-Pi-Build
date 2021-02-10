c02bf810f46da6229a86c7e4258ff4ddc52994f2
Tom Hocker <tomhocker@protonmail.com>
2021-02-10 10:37:12 -0500

    point updater to main branch

--- updater.sh
+++ updater.sh
@@ -6,10 +6,10 @@
 # as well as plugs in the most recent firefox bookmarks
 cd /home/osint
 
-wget -O updater-current.sh https://raw.githubusercontent.com/tracelabs/trace-labs-vm-ras-pi-build/humandecoded/updater-current.sh
+wget -O updater-current.sh https://raw.githubusercontent.com/tracelabs/trace-labs-vm-ras-pi-build/main/updater-current.sh
 chmod +x /home/osint/updater-current.sh
 sudo /home/osint/updater-current.sh
 
 sudo rm -f updater-current.sh
 
-wget -O bookmarks.html https://raw.githubusercontent.com/tracelabs/trace-labs-vm-ras-pi-build/humandecoded/bookmarks.html
+wget -O bookmarks.html https://raw.githubusercontent.com/tracelabs/trace-labs-vm-ras-pi-build/main/bookmarks.html
